9b185edce427bf93964d08450d08a40ee7552db1
Pilhwan Kim <pilhwankim03@gmail.com>
2020-10-22 08:56:17 +0900

    @Configuration과 바이트코드 조작의 마법

--- src/main/java/hello/springcore/AppConfig.java
+++ src/main/java/hello/springcore/AppConfig.java
@@ -16,27 +16,28 @@ import org.springframework.context.annotation.Configuration;
  * 객체의 생성과 연결(의존관계 주입, Dependency Injection)은 AppConfig 가 담당한다.
  * DIP 의 완성이 가능하려면 AppConfig 의 역할을 분리해야 한다.
  */
+// @Configuration 주석 처리하고 ConfigurationSingletonTest 실행결과 보기
 @Configuration
 public class AppConfig {
 
     @Bean
     public MemberService memberService() {
-        // 주석 풀고 ConfigurationSingletonTest.configurationTest 실행해서 확인해보자
-//        System.out.println("call AppConfig.memberService");
+        // 주석 풀고 ConfigurationSingletonTest 실행해서 확인해보자
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        // 주석 풀고 ConfigurationSingletonTest.configurationTest 실행해서 확인해보자
-//        System.out.println("call AppConfig.memberRepository");
+        // 주석 풀고 ConfigurationSingletonTest 실행해서 확인해보자
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
-        // 주석 풀고 ConfigurationSingletonTest.configurationTest 실행해서 확인해보자
-//        System.out.println("call AppConfig.orderService");
+        // 주석 풀고 ConfigurationSingletonTest 실행해서 확인해보자
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
